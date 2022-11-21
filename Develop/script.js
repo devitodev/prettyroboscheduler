0f4814d32668c286fa1593c1533aeca33f02dd10
devitodev <softwaredev.ito@gmail.com>
2022-11-21 13:46:42 -0600

    Added all of the values for storing local items

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,48 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+var saveButton = $('.saveBtn')
+
+// Getting the values from save button
+
+$(saveButton).click
+
+
+// Attempting to get current day to display
+var dateArea = $('#currentDay');
+var currentDate = dayjs()
+dateArea.text(currentDate)
+
+// Getting all local items
+$('#text9am').val(localStorage.text9am)
+$('#text10am').val(localStorage.text10am)
+$('#text11am').val(localStorage.text11am)
+$('#text12pm').val(localStorage.text12pm)
+$('#text1pm').val(localStorage.text1pm)
+$('#text2pm').val(localStorage.text2pm)
+$('#text3pm').val(localStorage.text3pm)
+$('#text4pm').val(localStorage.text4pm)
+$('#text5pm').val(localStorage.text5pm)
+
+// Adding functionality for all save buttons
+
+
+// Click for storing local data
+saveButton.on('click', storeData)
+
+
+
+
+
+// Attempting to store local saved items
+// function storeData() {
+//     var 
+// }
+
+
+
+// $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +61,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
